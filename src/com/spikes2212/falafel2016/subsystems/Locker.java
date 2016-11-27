6c558b6944235264e1f47418b1305dbf4fa51553
uriahrokach <uriahrokach@gmail.com>
2016-11-27 20:41:22 +0200

    Added Locker

---
+++ src/com/spikes2212/falafel2016/subsystems/Locker.java
@@ -0,0 +1,43 @@
+package com.spikes2212.falafel2016.subsystems;
+
+import com.spikes2212.genericsubsystems.LimitedSubsystem;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
+
+/**
+ *
+ */
+public class Locker extends LimitedSubsystem {
+
+	private DigitalInput up, down;
+
+	public Locker(SpeedController motor, DigitalInput up, DigitalInput down) {
+		super(motor);
+		this.up = up;
+		this.down = down;
+	}
+
+	public Locker(int motorPort, int upPort, int downPort) {
+		this(new VictorSP(motorPort), new DigitalInput(upPort), new DigitalInput(downPort));
+	}
+
+	public void initDefaultCommand() {
+
+	}
+
+	@Override
+	public boolean isMin() {
+
+		return down.get();
+	}
+
+	@Override
+	public boolean isMax() {
+
+		return up.get();
+	}
+
+}
