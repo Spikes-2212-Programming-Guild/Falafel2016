cfff34b853912905a5bfe664b33a057527cb5cfa
omri cohen <omrichoen@gmail.com>
2016-11-27 20:08:12 +0200

    subsystem Crane was added

---
+++ src/com/spikes2212/falafel2016/subsystems/Crane.java
@@ -0,0 +1,47 @@
+package com.spikes2212.falafel2016.subsystems;
+
+import com.spikes2212.genericsubsystems.LimitedSubsystem;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Crane extends LimitedSubsystem {
+	DigitalInput min;
+	DigitalInput max;
+	public Crane(SpeedController motor,DigitalInput min,DigitalInput max) {
+		super(motor);
+		this.min=min;
+		this.max=max;
+	}
+
+	
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
+	@Override
+	public boolean isMin() {
+		// TODO Auto-generated method stub
+		return min.get();
+	}
+
+	@Override
+	public boolean isMax() {
+		// TODO Auto-generated method stub
+		return max.get();
+	}
+
+	@Override
+	public PIDSource getPIDSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
