e7e78a43ee88f75dd649a98bfa5f3fb52d5e7add
omri cohen <omrichoen@gmail.com>
2016-11-27 21:23:22 +0200

    Crane was fixed

--- src/com/spikes2212/falafel2016/subsystems/Crane.java
+++ src/com/spikes2212/falafel2016/subsystems/Crane.java
@@ -1,41 +1,44 @@
 package com.spikes2212.falafel2016.subsystems;
 
+import com.spikes2212.falafel2016.RobotMap;
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 /**
  *
  */
 public class Crane extends LimitedSubsystem {
-	private DigitalInput min;
-	private DigitalInput max;
-	public Crane(SpeedController motor,DigitalInput min,DigitalInput max) {
+
+	public static final double MIN = 35, MAX = 180, LOAD = 90;
+	private Potentiometer pot;
+
+	public Crane(SpeedController motor, int port) {
 		super(motor);
-		this.min=min;
-		this.max=max;
+		this.pot = new AnalogPotentiometer(port, 360, 0);
 	}
 
-	
-
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 
 	@Override
 	public boolean isMin() {
 		// TODO Auto-generated method stub
-		return min.get();
+		return (pot.get() <= MIN);
 	}
 
 	@Override
 	public boolean isMax() {
 		// TODO Auto-generated method stub
-		return max.get();
+		return (pot.get() >= MAX);
 	}
 
 	@Override
@@ -43,5 +46,5 @@ public class Crane extends LimitedSubsystem {
 		// TODO Auto-generated method stub
 		return null;
 	}
-}
 
+}
