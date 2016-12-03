cdc679f247c75ec26aee024ba11ffd3e6302001c
omri cohen <omrichoen@gmail.com>
2016-12-03 12:54:21 +0200

    fold and score floopy were added

---
+++ src/com/spikes2212/falafel2016/commands/Fold.java
@@ -0,0 +1,18 @@
+package com.spikes2212.falafel2016.commands;
+
+import com.spikes2212.falafel2016.Robot;
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Fold extends CommandGroup {
+    static public final double CRANE_SPEED=0.5 , LOCKER_SPEED=0.3;
+    public  Fold() {
+    	addParallel(new MoveLimitedSubsystem(Robot.crane,CRANE_SPEED));
+    	addSequential(new MoveLimitedSubsystem(Robot.locker, LOCKER_SPEED));
+   
+    }
+}
