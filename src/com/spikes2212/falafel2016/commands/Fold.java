8a2387583d1df9011e4e3b32e990af2f8cb56659
omri cohen <omrichoen@gmail.com>
2016-12-03 15:51:08 +0200

    fix the speeds

--- src/com/spikes2212/falafel2016/commands/Fold.java
+++ src/com/spikes2212/falafel2016/commands/Fold.java
@@ -9,10 +9,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class Fold extends CommandGroup {
-    static public final double CRANE_SPEED=0.5 , LOCKER_SPEED=0.3;
+    static public final double CRANE_SPEED=-0.5 , LOCKER_SPEED=-0.3;
     public  Fold() {
     	addParallel(new MoveLimitedSubsystem(Robot.crane,CRANE_SPEED));
-    	addSequential(new MoveLimitedSubsystem(Robot.locker, LOCKER_SPEED));
+    	addParallel(new MoveLimitedSubsystem(Robot.locker, LOCKER_SPEED));
    
     }
 }
