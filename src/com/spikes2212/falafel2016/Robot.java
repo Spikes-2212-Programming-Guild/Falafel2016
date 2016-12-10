4e82d22032de32b17e16259ce216a5dae5f0d02d
Tomer <Tomer@Tomer-PC>
2016-12-10 16:10:29 +0200

    delete unnecessary imports

--- src/com/spikes2212/falafel2016/Robot.java
+++ src/com/spikes2212/falafel2016/Robot.java
@@ -2,9 +2,7 @@
 package com.spikes2212.falafel2016;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
