fe0081049c61c6d57c4447b922ee04b6ce2b6eeb
Tomer <Tomer@192.168.1.128>
2016-12-11 17:50:05 +0200

    Crane is now using digital inputs

--- src/com/spikes2212/falafel2016/subsystems/Crane.java
+++ src/com/spikes2212/falafel2016/subsystems/Crane.java
@@ -1,26 +1,28 @@
 package com.spikes2212.falafel2016.subsystems;
 
+
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
-/**
- *
- */
 public class Crane extends LimitedSubsystem {
 
 	public static final double MIN_ANGLE = 35, MAX_ANGLE = 180, LOAD_ANGLE = 90;
-	
+
 	public static final double CRANE_OPEN_SPEED = 0.5;
 	public static final double CRANE_CLOSING_SPEED = -0.5;
-	
-	private Potentiometer potentiometer;
 
-	public Crane(SpeedController motor,  Potentiometer potentiometer) {
+	private Potentiometer potentiometer;
+	private DigitalInput up,down;
+
+	public Crane(SpeedController motor, Potentiometer potentiometer,DigitalInput up,DigitalInput down) {
 		super(motor);
 		this.potentiometer = potentiometer;
+		this.up=up;
+		this.down=down;
 	}
 
 	public void initDefaultCommand() {
@@ -31,19 +33,19 @@ public class Crane extends LimitedSubsystem {
 	@Override
 	public boolean isMin() {
 		// TODO Auto-generated method stub
-		return (potentiometer.get() <= MIN_ANGLE);
+		return down.get();
 	}
 
 	@Override
 	public boolean isMax() {
 		// TODO Auto-generated method stub
-		return (potentiometer.get() >= MAX_ANGLE);
+		return up.get();
 	}
 
 	@Override
 	public PIDSource getPIDSource() {
 		// TODO Auto-generated method stub
-		return potentiometer;
+		return null;
 	}
 
 }
