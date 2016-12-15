fcf8027d67748c53bfba30c421414f78097ad441
Tomer Sitton <tomer.sitton1@gmail.com>
2016-12-15 17:37:23 +0200

    fixed moveToLoadPosition

--- src/com/spikes2212/falafel2016/commands/MoveToLoadPosition.java
+++ src/com/spikes2212/falafel2016/commands/MoveToLoadPosition.java
@@ -1,12 +1,7 @@
 package com.spikes2212.falafel2016.commands;
 
-import java.util.function.Supplier;
-
-import com.spikes2212.falafel2016.Robot;
-import com.spikes2212.falafel2016.RobotMap;
 import com.spikes2212.falafel2016.subsystems.Brake;
 import com.spikes2212.falafel2016.subsystems.Crane;
-import com.spikes2212.falafel2016.subsystems.Locker;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystemWithPID;
 
@@ -20,5 +15,6 @@ public class MoveToLoadPosition extends CommandGroup {
 	public MoveToLoadPosition(Crane crane) {
 		addSequential(new MoveLimitedSubsystem(crane.brake, Brake.OPEN_SPEED));
 		addSequential(new MoveLimitedSubsystemWithPID(Crane.LOAD_ANGLE));
+		addSequential(new MoveLimitedSubsystem(crane.brake, Brake.CLOSE_SPEED));
 	}
 }
