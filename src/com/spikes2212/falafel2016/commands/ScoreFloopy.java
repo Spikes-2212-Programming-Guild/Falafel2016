455d53ea01c216b047e0b14839179b6bc1b7d309
Tomer Sitton <tomer.sitton1@gmail.com>
2016-12-15 18:05:38 +0200

    unfold locker

--- src/com/spikes2212/falafel2016/commands/ScoreFloopy.java
+++ src/com/spikes2212/falafel2016/commands/ScoreFloopy.java
@@ -2,6 +2,8 @@ package com.spikes2212.falafel2016.commands;
 
 import com.spikes2212.falafel2016.Robot;
 import com.spikes2212.falafel2016.subsystems.Crane;
+import com.spikes2212.falafel2016.subsystems.Locker;
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -11,5 +13,6 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ScoreFloopy extends CommandGroup {
 	public ScoreFloopy() {
 		addSequential(new MoveCrane(Robot.crane, Crane.CRANE_OPEN_SPEED));
+		addSequential(new MoveLimitedSubsystem(Robot.locker, Locker.UNLOCKING_SPEED));
 	}
 }
