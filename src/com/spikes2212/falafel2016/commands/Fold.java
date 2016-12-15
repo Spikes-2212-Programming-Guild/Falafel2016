dde67824bdd52bc7b8214b43be1ad5578b0d5cb6
Tomer Sitton <tomer.sitton1@gmail.com>
2016-12-15 18:04:36 +0200

    fold locker

--- src/com/spikes2212/falafel2016/commands/Fold.java
+++ src/com/spikes2212/falafel2016/commands/Fold.java
@@ -2,7 +2,6 @@
 package com.spikes2212.falafel2016.commands;
 
 import com.spikes2212.falafel2016.Robot;
-import com.spikes2212.falafel2016.RobotMap;
 import com.spikes2212.falafel2016.subsystems.Crane;
 import com.spikes2212.falafel2016.subsystems.Locker;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
@@ -14,6 +13,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class Fold extends CommandGroup {
 	public Fold() {
+		addSequential(new MoveLimitedSubsystem(Robot.locker, Locker.LOCKING_SPEED));
 		addSequential(new MoveCrane(Robot.crane, Crane.CRANE_CLOSING_SPEED));
 	}
 }
