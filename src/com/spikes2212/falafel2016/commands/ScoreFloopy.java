aae53c56b7d6cedf1028d25577d01fdf71e6c6c6
Tomer Sitton <tomer.sitton1@gmail.com>
2016-12-15 17:38:09 +0200

    fixed scoreFloopy

--- src/com/spikes2212/falafel2016/commands/ScoreFloopy.java
+++ src/com/spikes2212/falafel2016/commands/ScoreFloopy.java
@@ -1,10 +1,7 @@
 package com.spikes2212.falafel2016.commands;
 
 import com.spikes2212.falafel2016.Robot;
-import com.spikes2212.falafel2016.RobotMap;
 import com.spikes2212.falafel2016.subsystems.Crane;
-import com.spikes2212.falafel2016.subsystems.Locker;
-import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -14,6 +11,5 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ScoreFloopy extends CommandGroup {
 	public ScoreFloopy() {
 		addSequential(new MoveCrane(Robot.crane, Crane.CRANE_OPEN_SPEED));
-		addSequential(new MoveLimitedSubsystem(Robot.locker, Locker.UNLOCKING_SPEED));
 	}
 }
