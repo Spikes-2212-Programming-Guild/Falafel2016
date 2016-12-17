33cf804ba91946a738e202462e32e66d0caffffd
omri cohen <omrichoen@gmail.com>
2016-12-17 15:51:55 +0200

    now extends DriveTank

--- src/com/spikes2212/falafel2016/commands/MoveToWall.java
+++ src/com/spikes2212/falafel2016/commands/MoveToWall.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 /**
  *
  */
-public class MoveToWall extends Command {
+public class MoveToWall extends DriveTank {
 
 	public static final double CAMERA_ANGLE=40;//FIXME actual number
 	public static final double CAMERA_HEIGHT_PIXELS=320;//FIXME actual number
@@ -24,34 +24,14 @@ public class MoveToWall extends Command {
 
 	
     public MoveToWall() {
-       requires(Robot.drivetrain);
+    	super(Robot.drivetrain,DRIVING_SPEED,DRIVING_SPEED);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    @Override
+    protected boolean isFinished() {
     	reflectivePixels=table.getNumber("",CAMERA_HEIGHT_PIXELS+10);//TODO:enter the name of this key 
     	cameraRange=(CAMERA_HEIGHT_PIXELS*REFLECTIVE_HEIGHT)/reflectivePixels; 
     	distance=cameraRange/(2*Math.tan((CAMERA_ANGLE/2)*Math.PI/180));
-    	if(distance<=MIN_RANGE);
-    	new DriveTank(Robot.drivetrain, DRIVING_SPEED, DRIVING_SPEED);	
-    	
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
+        return (Math.abs(distance)<=MIN_RANGE);
     }
 }
