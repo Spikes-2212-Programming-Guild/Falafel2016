182e50d8d49be3a41847eb70c0e9ec3fe8123332
roeeiit1 <roeeiit1@gmail.com>
2016-12-20 09:15:09 +0200

    Added ports to robot map and configured in robot

--- src/com/spikes2212/falafel2016/Robot.java
+++ src/com/spikes2212/falafel2016/Robot.java
@@ -1,7 +1,10 @@
 
 package com.spikes2212.falafel2016;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -9,9 +12,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.spikes2212.falafel2016.commands.MoveAndDiscontainFloopyAuto;
+import com.spikes2212.falafel2016.subsystems.Brake;
 import com.spikes2212.falafel2016.subsystems.Crane;
 import com.spikes2212.falafel2016.subsystems.Drivetrain;
 import com.spikes2212.falafel2016.subsystems.Locker;
+import com.spikes2212.utils.DoubleSpeedcontroller;
 
 public class Robot extends IterativeRobot {
 
@@ -27,7 +32,18 @@ public class Robot extends IterativeRobot {
 		chooser = new SendableChooser();
 		chooser.addDefault("MoveAndDiscontainFloopy", new MoveAndDiscontainFloopyAuto());
 		SmartDashboard.putData("auto choose", chooser);
-
+		drivetrain = new Drivetrain(
+				new DoubleSpeedcontroller(new VictorSP(RobotMap.PWM.DRIVETRAIN_RIGHT_1),
+						new VictorSP(RobotMap.PWM.DRIVETRAIN_RIGHT_2)),
+				new DoubleSpeedcontroller(new VictorSP(RobotMap.PWM.DRIVETRAIN_LEFT_1),
+						new VictorSP(RobotMap.PWM.DRIVETRAIN_LEFT_2)));
+		Brake breaker = new Brake(new VictorSP(RobotMap.PWM.BREAK), new DigitalInput(RobotMap.DIO.BREAK_CLOSED),
+				new DigitalInput(RobotMap.DIO.BREAK_CLOSED));
+		crane = new Crane(
+				new DoubleSpeedcontroller(new CANTalon(RobotMap.CAN.CRANE_1), new CANTalon(RobotMap.CAN.CRANE_2)), null,
+				new DigitalInput(RobotMap.DIO.CRANE_UP), new DigitalInput(RobotMap.DIO.CRANE_DOWN), breaker);
+		locker = new Locker(new VictorSP(RobotMap.PWM.LOCKER), new DigitalInput(RobotMap.DIO.LOCKER_OPEN),
+				new DigitalInput(RobotMap.DIO.LOCKER_CLOSED));
 		oi = new OI();
 	}
 
--- src/com/spikes2212/falafel2016/RobotMap.java
+++ src/com/spikes2212/falafel2016/RobotMap.java
@@ -11,14 +11,27 @@ public class RobotMap {
 	}
 
 	public interface CAN {
+		public static final int CRANE_1 = 3;
+		public static final int CRANE_2 = 1;
 	}
 
 	public interface PWM {
+		public static final int DRIVETRAIN_RIGHT_1 = 0;
+		public static final int DRIVETRAIN_RIGHT_2 = 1;
+		public static final int DRIVETRAIN_LEFT_1 = 2;
+		public static final int DRIVETRAIN_LEFT_2 = 3;
+		public static final int LOCKER = 4;
+		public static final int BREAK = 5;
 	}
 
 	public interface DIO {
+		public static final int CRANE_UP = 9;//Dummy Port
+		public static final int LOCKER_OPEN = 0;// Electrical name - No Name
+		public static final int CRANE_DOWN = 1;// Electrical name - LSSUP
+		public static final int BREAK_CLOSED = 2;// Electrical name - LSMLC
+		public static final int BREAK_OPEN = 3;// Electrical name - LSMLO
+		public static final int LOCKER_CLOSED = 4;// Electrical name - LSAC
 	}
-
 	public interface AnalogInput {
 	}
 }
