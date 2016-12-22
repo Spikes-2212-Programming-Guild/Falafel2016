10fabab137101c824dfaba44c634ba7b06a3bccf
roeeiit1 <roeeiit1@gmail.com>
2016-12-22 20:51:31 +0200

    simplified isfinished

--- src/com/spikes2212/falafel2016/commands/MakeLockerStickToLimit.java
+++ src/com/spikes2212/falafel2016/commands/MakeLockerStickToLimit.java
@@ -1,24 +1,16 @@
 package com.spikes2212.falafel2016.commands;
 
-import java.sql.Time;
-
 import com.spikes2212.falafel2016.Robot;
-import com.spikes2212.falafel2016.subsystems.Locker;
-import com.spikes2212.genericsubsystems.LimitedSubsystem;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class MakeLockerStickToLimit extends MoveLimitedSubsystem {
 	public static final double WAITING_TIME = 5;
-	private double firstTimeHitLimits;
-	private double currentTime;
-	private Timer t = new Timer();
-	private boolean stillHitting = false;
+	private double lastFalseTime = 0;
 
 	public MakeLockerStickToLimit(double speed) {
 		super(Robot.locker, speed);
@@ -28,26 +20,14 @@ public class MakeLockerStickToLimit extends MoveLimitedSubsystem {
 
 	@Override
 	protected boolean isFinished() {
-		currentTime = t.get();
-		if (Robot.locker.isMax() || Robot.locker.isMin()) {
-			
-			if (!stillHitting) {
-				firstTimeHitLimits = currentTime;
-				stillHitting = true;
-				return false;
-			}
-			
-			if (currentTime - firstTimeHitLimits >= WAITING_TIME) {
-				stillHitting = false;
-				return true;
-			}
-			
-			return false;
+		double currentTime = Timer.getMatchTime();
+		if (!super.isFinished()) {
+			lastFalseTime = currentTime;
 		}
-		
-		stillHitting = false;
+		if (currentTime - lastFalseTime > WAITING_TIME) {
+			return true;
+		}
 		return false;
-
 	}
 
 }
