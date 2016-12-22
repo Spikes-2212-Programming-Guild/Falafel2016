dc27cd2683d4b9d2f0f9d14dc6e3058fc7d248ca
Spikes Programming Guild <spikes.no2212@gmail.com>
2016-12-22 19:55:34 +0200

    Lowred crane opening timeout

--- src/com/spikes2212/falafel2016/commands/MoveToStartingPosition.java
+++ src/com/spikes2212/falafel2016/commands/MoveToStartingPosition.java
@@ -6,7 +6,7 @@ import com.spikes2212.falafel2016.subsystems.Crane;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class MoveToStartingPosition extends CommandGroup {
-	public static final double OPENING_TIME_OUT = 0.5;
+	public static final double OPENING_TIME_OUT = 1;
 
 	public MoveToStartingPosition() {
 		addSequential(new MoveCrane(Robot.crane, Crane.CRANE_OPEN_SPEED, OPENING_TIME_OUT));
