d79f50afd3ee75cc2af65d6e247d037a2fa7077c
Tomer Sitton <tomer.sitton1@gmail.com>
2016-12-23 15:05:57 +0200

    used MakeLockerStickToLimit into ScoreFloopy

--- src/com/spikes2212/falafel2016/commands/ScoreFloopy.java
+++ src/com/spikes2212/falafel2016/commands/ScoreFloopy.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ScoreFloopy extends CommandGroup {
 	public ScoreFloopy() {
 		addSequential(new MoveCrane(Robot.crane, Crane.CRANE_OPEN_SPEED));
-		addSequential(new MoveLimitedSubsystem(Robot.locker, Locker.UNLOCKING_SPEED));
+//		addSequential(new MoveLimitedSubsystem(Robot.locker, Locker.UNLOCKING_SPEED));
+		addSequential(new MakeLockerStickToLimit(Locker.UNLOCKING_SPEED));
 	}
 }
