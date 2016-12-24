82a906d2e5bc666b6d1b8bd8439ec857f874e298
ThinkRedstone <ittai11@gmail.com>
2016-12-24 12:03:12 +0200

    overrode isFinished method

--- src/com/spikes2212/falafel2016/commands/MoveCrane.java
+++ src/com/spikes2212/falafel2016/commands/MoveCrane.java
@@ -7,25 +7,47 @@ import com.spikes2212.falafel2016.subsystems.Crane;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class MoveCrane extends CommandGroup {
-	public MoveCrane(Crane crane, Supplier<Double> s) {
-		addSequential(new MoveLimitedSubsystem(crane.brake, Brake.OPEN_SPEED));
-		addSequential(new MoveLimitedSubsystem(crane, s));
-		addSequential(new MoveLimitedSubsystem(crane.brake, Brake.CLOSE_SPEED));
-	}
+    Supplier<Double> speedSupplier;
+    Crane crane;
+    public final static double waitTimeout = 0.2;
 
-	public MoveCrane(Crane crane, Supplier<Double> s, double timeout) {
-		addSequential(new MoveLimitedSubsystem(crane.brake, Brake.OPEN_SPEED));
-		addSequential(new MoveLimitedSubsystem(crane, s),timeout);//add timeut
-		addSequential(new MoveLimitedSubsystem(crane.brake, Brake.CLOSE_SPEED));
-	}
+    public MoveCrane(Crane crane, Supplier<Double> s) {
+        this.crane = crane;
+        speedSupplier = s;
+        addSequential(new WaitCommand(waitTimeout));
+        addSequential(new MoveLimitedSubsystem(crane.brake, Brake.OPEN_SPEED));
+        addSequential(new MoveLimitedSubsystem(crane, s));
+        addSequential(new MoveLimitedSubsystem(crane.brake, Brake.CLOSE_SPEED));
+    }
 
-	public MoveCrane(Crane crane, double speed) {
-		this(crane, () -> speed);
-	}
+    public MoveCrane(Crane crane, Supplier<Double> s, double timeout) {
+        this.crane = crane;
+        speedSupplier = s;
+        addSequential(new WaitCommand(waitTimeout));
+        addSequential(new MoveLimitedSubsystem(crane.brake, Brake.OPEN_SPEED));
+        addSequential(new MoveLimitedSubsystem(crane, s), timeout);//add timeut
+        addSequential(new MoveLimitedSubsystem(crane.brake, Brake.CLOSE_SPEED));
+    }
 
-	public MoveCrane(Crane crane, double speed, double timeout) {
-		this(crane, () -> speed, timeout);
-	}
+    public MoveCrane(Crane crane, double speed) {
+        this(crane, () -> speed);
+    }
+
+    public MoveCrane(Crane crane, double speed, double timeout) {
+        this(crane, () -> speed, timeout);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        if (crane.isUp() && speedSupplier.get() > 0) {
+            return true;
+        }
+        if (crane.isDown() && speedSupplier.get() < 0) {
+            return true;
+        }
+        return super.isFinished();
+    }
 }
