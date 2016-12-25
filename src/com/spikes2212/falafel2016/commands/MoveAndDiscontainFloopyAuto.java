65b9943d5e33dceebbc839d31fd651a02a63abe9
ThinkRedstone <ittai11@gmail.com>
2016-12-25 11:06:26 +0200

    Added auto to only move to wall

--- src/com/spikes2212/falafel2016/commands/MoveAndDiscontainFloopyAuto.java
+++ src/com/spikes2212/falafel2016/commands/MoveAndDiscontainFloopyAuto.java
@@ -7,13 +7,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class MoveAndDiscontainFloopyAuto extends CommandGroup {
-    public static final double LEFT_SPEED = 0.3;
-    public static final double RIGHT_SPEED = 0.3;
-    public static final double MOVING_TIME_OUT = 7;
     public static final double DROPPING_TIME_OUT = 3;
 
     public MoveAndDiscontainFloopyAuto() {
-        addSequential(new DriveTank(Robot.drivetrain, LEFT_SPEED, RIGHT_SPEED), MOVING_TIME_OUT);
+        addSequential(new MoveAndDiscontainFloopyAuto());
         addSequential(new ScoreFloopy());
         addSequential(new WaitCommand(DROPPING_TIME_OUT));
         addSequential(new MoveCrane(Robot.crane, Robot.crane.CRANE_CLOSE_SUPPLIER));
