49071eda183b9f5973fd01cd600d87da569d1d65
Spikes Programming Guild <spikes.no2212@gmail.com>
2016-12-26 19:35:26 +0200

    finalized auto code. also we got to the Final!

--- src/com/spikes2212/falafel2016/commands/MoveToWallByTimeAuto.java
+++ src/com/spikes2212/falafel2016/commands/MoveToWallByTimeAuto.java
@@ -8,9 +8,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  * Created by thinkredstone on 12/25/16.
  */
 public class MoveToWallByTimeAuto extends CommandGroup {
-    public static final double LEFT_SPEED = 0.5;
-    public static final double RIGHT_SPEED = 0.5;
-    public static final double MOVING_TIME_OUT = 5;
+    public static final double LEFT_SPEED = 0.4;
+    public static final double RIGHT_SPEED = 0.4;
+    public static final double MOVING_TIME_OUT = 4;
 
     public MoveToWallByTimeAuto() {
         addSequential(new DriveTank(Robot.drivetrain, LEFT_SPEED, RIGHT_SPEED), MOVING_TIME_OUT);
